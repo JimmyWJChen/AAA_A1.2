a975e7563c9b55c4bb2ca675124fdee50fb57b26
JimmyWJChen <W.J.Chen-1@student.tudelft.nl>
2025-05-25 03:34:50 +0200

    Old changes based on outdated version of main.

--- main.py
+++ main.py
@@ -2,25 +2,32 @@ import AAA
 import AAA.Qmatrix
 import AAA.plotting
 import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == "__main__":
     # Geometry terms
-    a = -0.2                # [-]       Distance center of airfoil to elastic axis
+    # [-]       Distance center of airfoil to elastic axis
+    a = -0.2
     b = 0.5                 # [m]       Semichord length
-    c = 0.5                 # [-]       Distance center of airfoil to hinge point
-    
+    # [-]       Distance center of airfoil to hinge point
+    c = 0.5
+
     # Inertia terms
     m = 50                  # [kg/m]    Mass per unit length
-    S = 5                   # [kgm/m]   Static mass moment of the wing around x_f
-    S_β = 1.56              # [kgm/m]   Static mass moment of the control surface around x_f
+    # [kgm/m]   Static mass moment of the wing around x_f
+    S = 5
+    # [kgm/m]   Static mass moment of the control surface around x_f
+    S_β = 1.56
     I_α = 4.67              # [kgm^2/m] Mass moment of the wing around x_f
-    I_αβ = 0.81             # [kgm^2/m] Product of inertia of the control surface
+    # [kgm^2/m] Product of inertia of the control surface
+    I_αβ = 0.81
     I_β = 0.26              # [kgm^2/m] Mass moment of the control surface
 
     # Structural stiffness components
     K_h = 25e3              # [N/m]     Linear heave stiffness
     K_α = 9e3               # [Nm/rad]  Linear pitch stiffness
     K_β = 1e3               # [Nm/rad]  Linear control stiffness
+    K_h7 = 100 * K_h        # [N/m^7]   Non-linear heave stiffness
 
     # Structural damping components
     C_h = K_h / 1000        # [Ns/m]    Structural heave damping
@@ -29,17 +36,33 @@ if __name__ == "__main__":
 
     # Flight conditions
     ρ = 1.225
-    v = 67 
-    v_max = 80 # For linear plots
+    v = 67
+    v_max = 80  # For linear plots
 
-    structural_section = AAA.Qmatrix.StructuralSection(a, b, c, m, S, S_β, I_α, I_αβ, I_β, C_h, C_α, C_β, K_h, K_α, K_β)
-    aeroelastic_section = AAA.Qmatrix.AeroelasticSection(structural_section, ρ, v)
+    structural_section = AAA.Qmatrix.StructuralSection(
+        a, b, c, m, S, S_β, I_α, I_αβ, I_β, C_h, C_α, C_β, K_h, K_α, K_β)
+    aeroelastic_section = AAA.Qmatrix.AeroelasticSection(
+        structural_section, ρ, v)
     Q = AAA.Qmatrix.get_Q_matrix(aeroelastic_section, Jones=False)
 
     print(structural_section.M_s)
     print(structural_section.K_s)
     print(structural_section.C_s)
+    # print(Q)
 
-    AAA.plotting.plot_uncoupled_structural_eigenmodes(structural_section, "output/linear/uncoupled_undamped_eigenmodes.pdf", heavemultiplier=0.5, ylim = [-0.7, 0.6])
-    AAA.plotting.plot_coupled_structural_eigenmodes(structural_section, "output/linear/coupled_undamped_eigenmodes.pdf", heavemultiplier=0.5, ylim = [-0.7, 0.6])
-    AAA.plotting.linear_flutter_diagrams(structural_section, v_max, ρ, "output/linear/velocity_against_eigenvalues.pdf", "output/linear/real_against_imaginary.pdf")
+    # AAA.plotting.plot_uncoupled_structural_eigenmodes(
+    #     structural_section, "output/linear/uncoupled_undamped_eigenmodes.pdf", heavemultiplier=0.5, ylim=[-0.7, 0.6])
+    # AAA.plotting.plot_coupled_structural_eigenmodes(
+    #     structural_section, "output/linear/coupled_undamped_eigenmodes.pdf", heavemultiplier=0.5, ylim=[-0.7, 0.6])
+    # AAA.plotting.linear_flutter_diagrams(
+    #     structural_section, v_max, ρ, "output/linear/velocity_against_eigenvalues.pdf", "output/linear/real_against_imaginary.pdf")
+
+    A_h = 0.1
+    SectionNL = AAA.Qmatrix.StructuralSection(
+        a, b, c, m, S, S_β, I_α, I_αβ, I_β, C_h, C_α, C_β, K_h, K_α, K_β, K_h7, A_h, True)
+    print(SectionNL.K_s)
+    print(SectionNL.K_h7_equivalent)
+    AESectionNL = AAA.Qmatrix.AeroelasticSection(SectionNL, ρ, v)
+    Qeqlin = AAA.Qmatrix.get_Q_matrix(AESectionNL, False)
+    # print(Qeqlin)
+    # assert np.allclose(Q, Qeqlin)
